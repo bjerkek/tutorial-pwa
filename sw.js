f6c2c66acaa24e8d59de362842400a690562cc7a
bjerkek <bjerkek@gmail.com>
2019-09-02 11:45:55 +0200

    Added manifest, service worker, etc

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Note that while this code works for demonstration purposes
+// it is not intended for production as it will stop if it
+// fails to load even one of the files.
+
+var cacheName = 'hello-pwa'
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+]
+
+// Start the service worker and cache all of the app's content
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+// Serve cached content when offline
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request)
+    })
+  )
+})
